7aed61d4ad1ed371d00d48920976948ac228e33f
sahle-1996 <sahlesiumgoitom@gmai.com>
2024-12-09 09:25:26 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+  // plugins: [
+  //   require('@tailwindcss/line-clamp'),
+  // ],
 };
